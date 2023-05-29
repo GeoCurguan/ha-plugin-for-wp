7e987ab0d46f499aaecb29c0efa489c1ed89a9f6
Geovanni Curguan <81426443+GeoCurguan@users.noreply.github.com>
2023-05-29 12:07:25 -0400

    hola mundo desde el dashboard

--- plugin/ha-plugin.php
+++ plugin/ha-plugin.php
@@ -9,4 +9,13 @@ Author URI: URL de tu sitio web o empresa
 License: Licencia del plugin
 */
 
+function hola_mundo(){
+  echo 'Hola desde el Dashboard de Wordpress';
+}
+
+function registrar_widget_hola_mundo() {
+    wp_add_dashboard_widget('widget_hola_mundo', 'Mi Widget Hola Mundo', 'hola_mundo');
+}
+add_action('wp_dashboard_setup', 'registrar_widget_hola_mundo');
+
 ?>
