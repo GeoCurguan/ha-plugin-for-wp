473dd3243d744db67f3f44780302561c739dc5b1
Geovanni Curguan <81426443+GeoCurguan@users.noreply.github.com>
2023-05-29 12:00:12 -0400

    Creacion de la base del plugin

---
+++ plugin/ha-plugin.php
@@ -0,0 +1,12 @@
+<?php
+/*
+Plugin Name: HA Plugin
+Plugin URI: URL del sitio web del plugin
+Description: Conecta los datos de tu agencia en HeyAndes con tu sitio de Wordpress
+Version: 1.0.0
+Author: Hey Andes SPA
+Author URI: URL de tu sitio web o empresa
+License: Licencia del plugin
+*/
+
+?>
