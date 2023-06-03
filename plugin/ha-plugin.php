f8543c994f2224fe7400a3e8291160f2718ec87a
Geovanni Curguan <81426443+GeoCurguan@users.noreply.github.com>
2023-06-03 17:37:17 -0400

    Inserta o Actualiza la información Precio de Experiencias

--- plugin/ha-plugin.php
+++ plugin/ha-plugin.php
@@ -10,59 +10,100 @@ License: Licencia del plugin
 */
 
 function experiencies_management_main(){
-	   if (isset($_POST['save_agency'])) {
+	if (isset($_POST['save_agency'])) {
 		// Guardar el valor del input en la opción 'dato_guardado'
 		update_option('key_agency', sanitize_text_field($_POST['agency_key']));
 		echo '<div class="updated"><p>Valor guardado correctamente.</p></div>';
-	    }
-    
+	}
+    global $wpdb;
+    $table_name = $wpdb->prefix . 'agency_experiences_data';
+    $table_exists = $wpdb->get_var("SHOW TABLES LIKE '$table_name'");
+    //Creamos la tabla para almacenar la data para los shortcodes
+    if ($table_exists !== $table_name) {
+        $query = "CREATE TABLE $table_name (
+            meta_id BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT,
+            experience_key VARCHAR(255) NOT NULL,
+            meta_key VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+            meta_value LONGTEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+            PRIMARY KEY (meta_id)
+        ) ENGINE = InnoDB;";
+        $wpdb->query($query);
+    }
+
 	$agency_key = get_option('key_agency');
 	if($agency_key){
 		//Mejorar a que esta información sólo se guarde cuando se conecta con la key, y no cada vez que ingresa al dashboard
-		$url = "https://firestore.googleapis.com/v1/projects/heyandes-web/databases/(default)/documents/agency/" . $agency_key;
-		$json_data = file_get_contents($url);
-		$data = json_decode($json_data);	
+		//$url = "https://firestore.googleapis.com/v1/projects/heyandes-web/databases/(default)/documents/agency/" . $agency_key . "/experiences";
+        $url = "https://firestore.googleapis.com/v1/projects/heyandesbooker-88007/databases/(default)/documents/agency/" . $agency_key . "/experiences";
+        $json_data = file_get_contents($url);
+		$data = json_decode($json_data);
 		if(isset($data)){
-			echo "Agencia conectada correctamente";
-			global $wpdb;
-			$new_table = $wpdb->prefix . 'uploaded_experiencies';
-			
-			//Mejorar consulta para que no compare con cada ejecucion
-			$create_table = 'CREATE TABLE IF NOT EXISTS  $new_table(
-				id INT,
-				key_experience VARCHAR(255),
-				meta_data LONGTEXT
-			)';
-			
-			//$resultados = $wpdb->get_results("INSERT INTO {$wpdb->prefix}uploaded_experieces");
-			var_dump($resultados);
-			//$agency_name = $data->fields->name->stringValue;
+			echo "Agencia conectada Correctamente <br>";
+
+			foreach ($data->documents as $document) {
+				//Acá recolectamos la información de las experiencias para subirla a la base de datos de Wordpress
+				$fields = $document->fields;
+				$condition = ($fields->key->stringValue != "string") &&
+                (isset($fields->isDisable->booleanValue) ? ($fields->isDisable->booleanValue === false) : true) &&
+                (isset($fields->isActive->booleanValue) ? ($fields->isActive->booleanValue === true) : true);
+                if($condition){
+                    $experience_key = $fields->key->stringValue;
+                    $experience_price = $fields->valuePerPerson->integerValue;
+                    $experience_name = $fields->name->stringValue;
+                    $rows = array(
+                        array('meta_key' => 'ha_experience_price', 'meta_value' => $experience_price),
+                        array('meta_key' => 'ha_experience_name', 'meta_value' => $experience_name)
+                    );
+
+                    foreach($rows as $row){
+                        $existing_row = $wpdb->get_row(
+                            $wpdb->prepare(
+                                "SELECT * FROM $table_name WHERE experience_key = %s AND meta_key = %s",
+                                $experience_key,
+                                $row['meta_key']
+                            )
+                        );
+                        if($existing_row){
+                            $wpdb->update(
+                                $table_name,
+                                array('meta_value' => $row['meta_value']),
+                                array('meta_id' => $existing_row->meta_id)
+                            );
+                        } else{
+                            $wpdb->insert(
+                                $table_name,
+                                array(
+                                    'experience_key' => $experience_key,
+                                    'meta_key' => $row['meta_key'],
+                                    'meta_value' => $row['meta_value']
+                                )
+                            );
+                        }
+                    }
+                }
+			}
 		}else{
-		
 			echo "Key de Agencia No registrada <br>";
 		}
-		//echo $agency_name;
 	}
-    
-    	echo '<form method="post" action="">';
-    	echo '<label for="dato_input">Key de agencia: </label>';
-        echo '<input type="text" id="agency_key" name="agency_key" value="' . esc_attr($agency_key) . '" />';
-        echo '<input type="submit" name="save_agency" value="Guardar" class="button button-primary" /><br>';
-        //Crear boton para que actualice y no ejecute cada vez que se abra la pestaña
-        
-    	echo '</form>';
+    echo '<form method="post" action="">';
+    echo '<label for="dato_input">Key de agencia: </label>';
+    echo '<input type="text" id="agency_key" name="agency_key" value="' . esc_attr($agency_key) . '" />';
+    echo '<input type="submit" name="save_agency" value="Guardar" class="button button-primary" /><br>';
+    //Crear boton para que actualice y no ejecute cada vez que se abra la pestaña
+    echo '</form>';
 }
 
 function agregar_pagina_menu() {
-  add_menu_page(
-      'HeyAndes Dashboard', // Título de la página
-      'HeyAndes Dashboard', // Texto del menú
-      'manage_options', // Capacidad requerida para verla
-      'mi-plugin', // Slug
-      'experiencies_management_main', // Nombre de la función de devolución de llamada
-      'dashicons-admin-plugins',
-      99 // Posición
-  );
+    add_menu_page(
+            'HeyAndes Dashboard', // Título de la página
+            'HeyAndes Dashboard', // Texto del menú
+            'manage_options', // Capacidad requerida para verla
+            'mi-plugin', // Slug
+            'experiencies_management_main', // Nombre de la función de devolución de llamada
+            'dashicons-admin-plugins',
+        99 // Posición
+    );
 }
 add_action('admin_menu', 'agregar_pagina_menu');
 
