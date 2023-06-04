08b8d3358f1afd730af7756577cf6aa52ccd6555
Geovanni Curguan <81426443+GeoCurguan@users.noreply.github.com>
2023-06-04 03:53:58 -0400

    Ahora obtiene precios y se le puede dar un estilo de salida

--- plugin/shortcodes/shortcodes.php
+++ plugin/shortcodes/shortcodes.php
@@ -1,8 +1,31 @@
 <?php
-function get_experience_price(){
-    return 'This is price';
+function get_experience_price($atts){
+    $atts = shortcode_atts(array(
+            'key' => '',
+            'style' => 'normal'
+    ), $atts);
+    global $wpdb;
+    $table_name = $wpdb->prefix . 'agency_experiences_data';
+    $existing_row = $wpdb->get_row(
+        $wpdb->prepare(
+            "SELECT meta_value FROM $table_name WHERE experience_key = %s AND meta_key = 'ha_experience_price'",
+            $atts['key'],
+        ));
+    if($existing_row){
+        //Podria trabajarse un string que se vaya concatenando para cada caso (si es que sequiere usar sub por ejemplo), para hacerlo limpio
+        if($atts['style'] == 'normal'){
+            return '$' . number_format($existing_row->meta_value,0,',','.');
+        }else{
+            return '$'. number_format($existing_row->meta_value,0,',','.') . $atts['style'] ;
+        }
+    }else{
+        return;
+    }
+
 }
 function get_experience_name(){
+    global $wpdb;
+    $table_name = $wpdb->prefix . 'agency_experiences_data';
     return 'This is a experience name';
 }
 
